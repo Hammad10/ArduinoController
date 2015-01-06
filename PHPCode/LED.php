f50a7dd7f2ec61b93e1a563807d599c1fdec80b8
hammad10 <hammad.mirza@hotmail.com>
2015-01-06 16:54:52 -0500

    Added comments to the PHP file

--- PHPCode/LED.php
+++ PHPCode/LED.php
@@ -1,7 +1,9 @@
 <?php
+	# Most of this PHP code taken from: http://sampleprogramz.com/android/mysqldb.php
+
 	$host='xxxx';
 	$uname='xxxx';
-	$pwd='xxxx';
+	$pwd='xxxx!';
 	$db="xxxx";
 
 	$con = mysql_connect($host,$uname,$pwd) or die("connection failed");
@@ -12,29 +14,36 @@
 	$length=$_REQUEST['length'];
 
 	$flag['code']=0;
-//#cur.execute(" UPDATE LED SET ArduinoPin = '11', LEDIntensity = '11', Length = '11' WHERE id = '1' ") ##Inserts into id1
-	//if($r=mysql_query("insert into Length values('$id','$name') ",$con))
+
+	# Insert ArduinoPin value
 	if($r=mysql_query("UPDATE LED SET ArduinoPin = '$pin' WHERE id = '1' ",$con))
 	{
 		$flag['code']=1;
 		echo"hi";
 	}
+	
+	# Insert LEDIntensity value
 	if($r=mysql_query("UPDATE LED SET LEDIntensity = '$intensity' WHERE id = '1' ",$con))
 	{
 		$flag['code']=1;
 		echo"hi";
 	}
+	
+	# Insert Length value
 	if($r=mysql_query("UPDATE LED SET Length = '$length' WHERE id = '1' ",$con))
 	{
 		$flag['code']=1;
 		echo"hi";
 	}
+	
+	# Insert readIndicator value
 	if($r=mysql_query("UPDATE LED SET readIndicator = '1' WHERE id = '1' ",$con))
 	{
 		$flag['code']=1;
 		echo"hi";
 	}
 
+	# Need to implement check for successful DB insertion
 	print(json_encode($flag));
 	mysql_close($con);
 ?>
